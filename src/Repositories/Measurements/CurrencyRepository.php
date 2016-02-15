cc1aa9cc9f2c77296f9cd12b771476450b26616a
james weston <jweston@fulfillment.com>
2016-02-14 22:44:20 -0500

    Inclusion of helper functions

--- src/Repositories/Measurements/CurrencyRepository.php
+++ src/Repositories/Measurements/CurrencyRepository.php
@@ -2,6 +2,7 @@
 namespace app\Repositories\Measurements;
 
 
+use app\Models\Measurements\Currency;
 use app\Utilities\ArrayUtil;
 use LaravelDoctrine\ORM\Pagination\Paginatable;
 use Doctrine\ORM\Query;
@@ -12,4 +13,62 @@ class CurrencyRepository extends BaseMeasurementRepository
     use Paginatable;
 
 
+    /**
+     * Get a single Currency object by its id
+     * @param       int                 $id                 Id to query against
+     * @return      Currency|null
+     */
+    public function getOneById($id)
+    {
+        return $this->find($id);
+    }
+
+    /**
+     * Get a single Currency object by its name
+     * @param       string              $name               Name to query against
+     * @return      Currency|null
+     */
+    public function getOneByName($name)
+    {
+        return $this->findOneBy(['name' => $name]);
+    }
+
+    /**
+     * Get a single Currency object by its symbol
+     * @param       string              $symbol             Symbol to query against
+     * @return      Currency|null
+     */
+    public function getOneBySymbol($symbol)
+    {
+        return $this->findOneBy(['symbol' => $symbol]);
+    }
+
+
+
+    /**
+     * Get the CAD Currency object
+     * @return      Currency
+     */
+    public function getCAD()
+    {
+        return $this->find(27);
+    }
+
+    /**
+     * Get the Euro Currency object
+     * @return      Currency
+     */
+    public function getEuro()
+    {
+        return $this->find(47);
+    }
+
+    /**
+     * Get the USD Currency object
+     * @return      Currency
+     */
+    public function getUSD()
+    {
+        return $this->find(151);
+    }
 }
