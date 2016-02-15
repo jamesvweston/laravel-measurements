d0fd13f759b3c782b026950974280db57813dbd8
james weston <jweston@fulfillment.com>
2016-02-14 23:01:51 -0500

    Inclusion of helper functions

--- src/Repositories/Measurements/DistanceTypeRepository.php
+++ src/Repositories/Measurements/DistanceTypeRepository.php
@@ -3,7 +3,6 @@ namespace app\Repositories\Measurements;
 
 
 use app\Models\Measurements\DistanceType;
-use app\Utilities\ArrayUtil;
 use LaravelDoctrine\ORM\Pagination\Paginatable;
 use Doctrine\ORM\Query;
 
@@ -11,6 +10,28 @@ class DistanceTypeRepository extends BaseMeasurementRepository
 {
 
     use Paginatable;
+
+    /**
+     * Query against all fields
+     * @param       []                      $query              Values to query against
+     * @param       bool                    $ignorePagination   If true will not return pagination
+     * @param       int|null                $maxLimit           If provided limit is greater than this value, set is to this value
+     * @param       int|null                $maxPage            If the provided page is greater than this value, restrict it to this value
+     * @return      DistanceType[]|array|\Illuminate\Pagination\LengthAwarePaginator
+     */
+    function where($query, $ignorePagination = true, $maxLimit = 5000, $maxPage = 100) {
+        $pagination                 =   $this->buildPagination($query, $maxLimit, $maxPage);
+        $qb                         =   $this->_em->createQueryBuilder();
+
+        $qb->select(['distanceType']);
+        $qb->from('postage\Models\DistanceType', 'distanceType');
+        $qb->orderBy('distanceType.id', 'ASC');
+
+        if ($ignorePagination)
+            return $qb->getQuery()->getResult();
+        else
+            return $this->paginate($qb->getQuery(), $pagination['limit']);
+    }
 
 
     /**
