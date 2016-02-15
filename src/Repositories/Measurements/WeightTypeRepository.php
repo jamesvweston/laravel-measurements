c10f01a6adcb8c41916c08ce051729178681dd42
james weston <jweston@fulfillment.com>
2016-02-14 22:52:28 -0500

    Inclusion of helper functions

--- src/Repositories/Measurements/WeightTypeRepository.php
+++ src/Repositories/Measurements/WeightTypeRepository.php
@@ -2,6 +2,7 @@
 namespace app\Repositories\Measurements;
 
 
+use app\Models\Measurements\WeightType;
 use app\Utilities\ArrayUtil;
 use LaravelDoctrine\ORM\Pagination\Paginatable;
 use Doctrine\ORM\Query;
@@ -12,4 +13,71 @@ class WeightTypeRepository extends BaseMeasurementRepository
     use Paginatable;
 
 
+    /**
+     * Get a single WeightType object by its id
+     * @param       int                 $id                 Id to query against
+     * @return      WeightType|null
+     */
+    public function getOneById($id)
+    {
+        return $this->find($id);
+    }
+
+    /**
+     * Get a single WeightType object by its name
+     * @param       string              $name               Name to query against
+     * @return      WeightType|null
+     */
+    public function getOneByName($name)
+    {
+        return $this->findOneBy(['name' => $name]);
+    }
+
+    /**
+     * Get a single WeightType object by its symbol
+     * @param       string              $symbol             Symbol to query against
+     * @return      WeightType|null
+     */
+    public function getOneBySymbol($symbol)
+    {
+        return $this->findOneBy(['symbol' => $symbol]);
+    }
+
+
+
+    /**
+     * Get the Ounce WeightType object
+     * @return      WeightType
+     */
+    public function getOunce()
+    {
+        return $this->find(1);
+    }
+
+    /**
+     * Get the Pound WeightType object
+     * @return      WeightType
+     */
+    public function getPound()
+    {
+        return $this->find(2);
+    }
+
+    /**
+     * Get the Kilogram WeightType object
+     * @return      WeightType
+     */
+    public function getKilogram()
+    {
+        return $this->find(3);
+    }
+
+    /**
+     * Get the Gram WeightType object
+     * @return      WeightType
+     */
+    public function getGram()
+    {
+        return $this->find(4);
+    }
 }
